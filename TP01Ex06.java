5ba2dc2eb73b483ea183341f3c7af623b154ac37
Alice <133613280+alice-marinho@users.noreply.github.com>
2024-03-11 18:32:20 -0300

    Create TP01Ex06.java

---
+++ TP01Ex06.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/* Alice da Silva
+Matheus Leandro
+
+6) Calcular e exibir a média aritmética de quatro valores quaisquer que serão digitados.
+*/
+
+public class Main {
+   public static void main(String[] args) {
+       Scanner scan= new Scanner(System.in);
+     System.out.println("Exercicio 6");
+       System.out.println("Digite o primeiro número:");
+       int primeiroNum = scan.nextInt();
+       System.out.println("Digite o segundo número:");
+       int segundoNum = scan.nextInt();
+       System.out.println("Digite o terceiro número:");
+       int terceiroNum = scan.nextInt();
+       System.out.println("Digite o quarto número:");
+       int quartoNum = scan.nextInt();
+
+   }}
